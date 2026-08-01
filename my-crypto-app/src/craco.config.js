bb3a4c6517a083115b98c85a884da7b3a46ef11c
Abdussalam <abdussalam30.1995@gmail.com>
2026-08-01 20:44:28 +0800

    fix craco config

---
+++ my-crypto-app/src/craco.config.js
@@ -0,0 +1,38 @@
+const webpack = require('webpack');
+
+module.exports = {
+  webpack: {
+    configure: {
+      resolve: {
+        fallback: {
+          util: require.resolve('util/'),
+          vm: require.resolve('vm-browserify'),
+          path: require.resolve('path-browserify'),
+          os: require.resolve('os-browserify/browser'),
+          crypto: require.resolve('crypto-browserify'),
+          stream: require.resolve('stream-browserify'),
+          http: require.resolve('stream-http'),
+          https: require.resolve('https-browserify'),
+          assert: require.resolve('assert/'),
+          url: require.resolve('url/'),
+          querystring: require.resolve('querystring-es3'),
+          buffer: require.resolve('buffer/'),
+          process: require.resolve('process/browser.js'),
+          fs: false,
+          net: false,
+          tls: false,
+          zlib: false,
+          dns: false,
+          child_process: false,
+          async_hooks: false,
+        },
+      },
+      plugins: [
+        new webpack.ProvidePlugin({
+          process: 'process/browser.js',
+          Buffer: ['buffer', 'Buffer'],
+        }),
+      ],
+    },
+  },
+};
